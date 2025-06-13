17a635bfc4a925e78faeead4d1fef9d97e5e9c68
Nvsuhail <103936567+Nvsuhail@users.noreply.github.com>
2025-06-13 10:28:03 +0530

    Fix hydration mismatch by removing NoSSR wrapper

--- src/app/AppWrappers.tsx
+++ src/app/AppWrappers.tsx
@@ -7,15 +7,6 @@ import 'styles/Contact.css';
 import 'styles/MiniCalendar.css';
 import 'styles/index.css';
 
-import dynamic from 'next/dynamic';
-
-const _NoSSR = ({ children }) => <React.Fragment>{children}</React.Fragment>;
-
-const NoSSR = dynamic(() => Promise.resolve(_NoSSR), {
-  ssr: false,
-});
-
 export default function AppWrappers({ children }: { children: ReactNode }) {
-  // @ts-expect-error
-  return <NoSSR>{children}</NoSSR>;
+  return <>{children}</>;
 }
